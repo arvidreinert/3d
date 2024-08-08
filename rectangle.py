0849f684584e960e599e006b62f219f230a19b95
Arvid Reinert <168847324+arvidreinert@users.noreply.github.com>
2024-08-08 10:11:46 +0200

    masks

--- rectangle.py
+++ rectangle.py
@@ -138,3 +138,8 @@ class Rectangle():
             self.original_rect.set_alpha(self.transparency)
             self.rect = self.original_rect.copy()
             self.rect_rect = self.rect.get_rect(center=self.position)
+    def return_perfect_colission(self,other_rectangle):
+        my_mask = pygame.mask.from_surface(self.image)
+        other_mask = pygame.mask.from_surface(other_rectangle.image)
+        return my_mask.overlap(other_mask,(other_rectangle.get_pos[0]-self.get_pos()[0],other_rectangle.get_pos[1]-self.get_pos()[1]))
+        
